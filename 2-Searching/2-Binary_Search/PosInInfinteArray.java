5f351c9942d6b129cec613d6e78f69cb4df1e6b8
Satish Panchal <satishpanchal651@gmail.com>
2021-09-19 21:56:10 +0530

    Added Poition of Element in Infinte Array problem

---
+++ 2-Searching/2-Binary_Search/PosInInfinteArray.java
@@ -0,0 +1,43 @@
+// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+public class PosInInfinteArray {
+
+    public static void main(String args[]) {
+        int[] arr = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+                30 };
+        int target = 15;
+        System.out.println(ans(arr, target));
+    }
+
+    public static int ans(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+
+        while (target > arr[end]) {
+            int newStart = end + 1;
+            // double the box value
+            // end = previous end + size of box * 2
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+
+        return binarySearch(arr, target, start, end);
+    }
+
+    private static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+}
