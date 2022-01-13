0b812d004be5a37b5d31d385bc4f8b4d92897ba2
Satish Panchal <satishpanchal651@gmail.com>
2022-01-13 22:24:25 +0530

    Added comment in Insertion Sort

--- 3-Sorting/InsertionSort.java
+++ 3-Sorting/InsertionSort.java
@@ -8,7 +8,7 @@ public class InsertionSort {
     }
 
     public static void insertion(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) { // can write i <= arr.length - 2
             for (int j = i + 1; j > 0; j--) {
                 if (arr[j] < arr[j - 1]) {
                     swap(arr, j, j - 1);
