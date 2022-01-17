a07cec27f89af7e31765c42d858c5b41cccd3b74
Satish Panchal <satishpanchal651@gmail.com>
2022-01-17 20:50:06 +0530

    Leetcode 268. Missing Numbers

---
+++ 3-Sorting/MissingNums.java
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/missing-number/
+
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Input: nums = [3,0,1]
+// Output: 2
+// Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+public class MissingNums {
+    public static void main(String[] args) {
+        int[] arr = { 3, 0, 1 };
+        int ans = cyclic(arr);
+        System.out.println(ans);
+    }
+
+    public static int cyclic(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+
+        // case 2
+        return arr.length;
+
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
