78656b421bf0da9d5cf99934ec6ece46f581ecd0
Satish Panchal <satishpanchal651@gmail.com>
2022-01-20 23:12:04 +0530

    Leetcode 442. Find All DUplicates in an Array

---
+++ 3-Sorting/FindAllDuplicates.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+// 442. Find All Duplicates in an Array
+// Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+// You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+// Example 1:
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [2,3]
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicates {
+    public static void main(String[] args) {
+        int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        System.out.println(findDuplicates(arr));
+    }
+
+    public static List<Integer> findDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // find missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                ans.add(arr[index]);
+            }
+        }
+        return ans;
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
