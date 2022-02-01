debc313e1293779d695f4d3a78144f2aa4cbce47
unknown <satishpanchal651@gmail.com>
2022-02-01 21:02:57 +0530

    Pattern Program

--- 0.1 Pattern Programs/Patterns.java
+++ 0.1 Pattern Programs/Patterns.java
@@ -4,7 +4,7 @@ class Patterns {
     public static void main(String args[]) {
         // pattern1(4);
         // System.out.println();
-        pattern5(4);
+        pattern7(4);
     }
 
     static void pattern1(int n) {
@@ -72,9 +72,48 @@ class Patterns {
         // * *
         // *
         for (int row = 1; row < 2 * n; row++) {
-            int totalColsInRow = row > n ? 2 * n - row: row;
+            int totalColsInRow = row>n ? 2*n-row : row;
             for (int col = 0; col < totalColsInRow; col++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern6(int n) {
+    //      * 
+    //     * *
+    //    * * *
+    //   * * * *
+    //    * * *
+    //     * *
+    //      *
+        for (int row = 1; row < 2 * n; row++) {
+            int totalColsInRow = row>n ? 2*n-row : row;
+            int noOfSpaces = n - totalColsInRow;
+
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern7(int n) {
+
+        for (int row = 1; row <= n ; row++) {
+            for (int spaces = 0; spaces < n-row; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
             }
             System.out.println();
         }
