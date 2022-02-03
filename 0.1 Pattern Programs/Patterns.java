dbcb682b5bdb5a90e31388acc5947359a7858603
unknown <satishpanchal651@gmail.com>
2022-02-03 17:29:11 +0530

    Pattern Program

--- 0.1 Pattern Programs/Patterns.java
+++ 0.1 Pattern Programs/Patterns.java
@@ -4,7 +4,7 @@ class Patterns {
     public static void main(String args[]) {
         // pattern1(4);
         // System.out.println();
-        pattern7(4);
+        pattern8(4);
     }
 
     static void pattern1(int n) {
@@ -104,7 +104,10 @@ class Patterns {
     }
 
     static void pattern7(int n) {
-
+    //       1 
+    //     2 1 2
+    //   3 2 1 2 3
+    // 4 3 2 1 2 3 4
         for (int row = 1; row <= n ; row++) {
             for (int spaces = 0; spaces < n-row; spaces++) {
                 System.out.print("  ");
@@ -113,6 +116,31 @@ class Patterns {
                 System.out.print(col + " ");
             }
             for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    static void pattern8(int n){
+    //       1 
+    //     2 1 2
+    //   3 2 1 2 3
+    // 4 3 2 1 2 3 4
+    //   3 2 1 2 3
+    //     2 1 2
+    //       1
+        for (int row = 1; row <= 2*n ; row++) {
+            
+            int c = row > n ? 2*n-row: row ;
+
+            for (int spaces = 0; spaces < n-c; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
                 System.out.print(col + " ");
             }
             System.out.println();
