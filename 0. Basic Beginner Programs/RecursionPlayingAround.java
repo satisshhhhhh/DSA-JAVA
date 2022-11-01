f19dfda697bf75bb62f99937712b1bc2a5d7fb69
unknown <satishpanchal651@gmail.com>
2022-11-01 13:36:48 +0530

    Recurssion

---
+++ 0. Basic Beginner Programs/RecursionPlayingAround.java
@@ -0,0 +1,12 @@
+public class RecursionPlayingAround {
+    static void func(int n) {
+        if ( n == 0 ){
+            return;
+        }
+        func( n - 1);
+        System.out.println(n);   
+    }    
+    public static void main(String[] args) {
+        func(5);
+    }
+}
---
+++ 0. Basic Beginner Programs/SumOfDigitsUsingRecursion.java
@@ -0,0 +1,13 @@
+public class SumOfDigitsUsingRecursion {
+    public static void main(String[] args) {
+        int ans = digitSum(1234);
+        System.out.println(ans);
+    }
+    
+    static int digitSum(int n) {
+        if ( n == 0 ){
+            return 0;
+        }
+        return (n%10) + digitSum(n/10);
+    }
+}
